262265ce77a929a4a61b7484111ac9730f9136b6
David <david@localhost.localdomain>
2019-11-24 04:47:17 -0600

    Updates

---
+++ precache-manifest.d0789711d81c731de5478251546ba4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01eaeaa24ee15894eb9a9d18d782ff42",
+    "url": "/admin-veterinaria/index.html"
+  },
+  {
+    "revision": "c7a5e230cc244f45c022",
+    "url": "/admin-veterinaria/static/css/main.8320971e.chunk.css"
+  },
+  {
+    "revision": "4a0f7f9bd9f32ca9f35d",
+    "url": "/admin-veterinaria/static/js/2.ef279b46.chunk.js"
+  },
+  {
+    "revision": "c7a5e230cc244f45c022",
+    "url": "/admin-veterinaria/static/js/main.28e87462.chunk.js"
+  },
+  {
+    "revision": "3629c6cc060cf425ebe1",
+    "url": "/admin-veterinaria/static/js/runtime-main.bd3ce3ca.js"
+  }
+]);
